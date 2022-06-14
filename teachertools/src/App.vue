26055a67b90c04cf411ffc1d23cd5d09171d9ff1
Magdaena Rother <rother.magdalena@gmail.com>
2022-06-14 11:54:35 +0200

    Add basic quiz view

--- teachertools/src/App.vue
+++ teachertools/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/quiz">Quiz</router-link> |
+    <a id="academis" href="https://www.academis.eu/" target="_blank">Academis</a>
   </div>
   <router-view/>
 </template>
